ab8514686599b3f476db0f4bc1147d4df8d1eaaa
Kaya Jackson <amanij7@gmail.com>
2018-07-25 15:51:55 -0400

    create chat rooms 1

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -14,7 +14,24 @@ class RoomList extends Component {
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat(room) })
    });
+ }
+   handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newRoomName){
+      return newRoomName;}
+    const newRoom = { rooms: this.state.newRoomName};
+    this.roomsRef.push({rooms: this.state.newRoomName], newRoomName: ''});
+    }
+  handleChange(e){
+    this.setState({newRoomName: e.target.value})
+    );
   }
+  deleteRoom() {
+    const rooms = this.state.rooms.slice();
+    const filteredRooms = rooms.filter();
+    this.setState({ rooms:filteredRooms })
+  }
+
 
   render() {
     return (
@@ -22,8 +39,18 @@ class RoomList extends Component {
       {this.state.rooms.map((room, index) =>
         <div key={index}>{room.name}</div>
       )}
-     </section>
-    )
+      <div id="create-room">
+         <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text"
+           value={this.state.newRoomName}
+           onChange= { (e) => this.handleChange(e) } />
+          <input type="submit" value="Create"/>
+          <button type="button" onClick={() => this.deleteRoom()}>Delete</button>
+        </form>
+        </div>
+       </section>
+       );
   }
-}
- export default RoomList
+ }
+
+export default RoomList
