399fba121b8eb13e924e5803ef2cafdc58c3642e
Kaya Jackson <amanij7@gmail.com>
2018-08-27 20:48:12 -0400

    list messages 17

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -42,7 +42,9 @@ handleSubmit(e) {
 formatTime(time) {
   console.log(time);
   var date = new Date(time);
-  return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+  if( this.hours >12 )
+  { this.hours = this.hours - 12; }
+  return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + " " + (date.getHours() % 12)+ ":" + date.getMinutes();
 }
   deleteMessage(message) {
     this.messageRef.child(message.key).remove();
